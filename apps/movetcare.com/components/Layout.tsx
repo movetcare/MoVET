4b27c612e5da10f28ecbd5592b27ec5afaa8785e
Alex Rodriguez <alex@alpinealex.com>
2022-10-19 16:50:39 -0500

    🐛 Performance Improvments w/ Dynamic Import for Announcement Banner

--- apps/movetcare.com/components/Layout.tsx
+++ apps/movetcare.com/components/Layout.tsx
@@ -1,17 +1,22 @@
+import dynamic from "next/dynamic";
 import { Header } from "components/Header";
 import { Footer } from "components/Footer";
-import { AnnouncementBanner } from "ui";
-
+let AnnouncementBanner: any = null;
 const Layout = ({ children, announcement }: any) => {
+  if (announcement?.isActive) {
+    AnnouncementBanner = dynamic(() =>
+      import("ui").then((mod) => mod.AnnouncementBanner)
+    );
+  }
   return (
     <div className="flex flex-col items-center justify-center">
-      {announcement && (
+      {AnnouncementBanner && (
         <AnnouncementBanner announcement={announcement} layout="top" />
       )}
       <Header />
       <main className="w-full flex-1 overflow-x-hidden">{children}</main>
       <Footer />
-      {announcement && (
+      {AnnouncementBanner && (
         <AnnouncementBanner announcement={announcement} layout="bottom" />
       )}
     </div>
