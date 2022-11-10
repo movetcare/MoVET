553b6fbd282dd796c2eab27d695619f6b6102570
Alex Rodriguez <alex@alpinealex.com>
2022-11-10 10:27:12 -0600

    :bug: Build Fixes

---
+++ apps/app.movetcare.com/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+const DEBUG = true;
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    if (DEBUG) console.log("request.nextUrl", request.nextUrl);
+    const params = Object.fromEntries(
+      new URLSearchParams(request.nextUrl.search).entries()
+    );
+    if (DEBUG) console.log("params", params);
+    const linkParams = Object.fromEntries(
+      new URLSearchParams(
+        params.link
+          .replaceAll("http://localhost:3001/account/", "")
+          .replaceAll("https://app.movetcare.com/account/", "")
+      ).entries()
+    );
+    if (DEBUG) console.log("linkParams", linkParams);
+    if (linkParams?.mode === "signIn")
+      return NextResponse.redirect(
+        "http://" +
+          request.nextUrl.host +
+          `/account/?mode=${linkParams?.mode}&oobCode=${linkParams?.oobCode}&continueUrl=${linkParams?.continueUrl}&lang=${linkParams?.lang}&apiKey=${linkParams?.apiKey}`
+      );
+  }
+}
