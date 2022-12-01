2f692ce1ec03baec3b1fdd1519a51ff402671143
Alex Rodriguez <alex@alpinealex.com>
2022-12-01 10:53:21 -0600

    :art: Admin Layout Fix

--- apps/admin.movetcare.com/components/Layout.tsx
+++ apps/admin.movetcare.com/components/Layout.tsx
@@ -19,12 +19,12 @@ const Layout = ({ children }: any) => {
         <main
           className={
             isAuthPage
-              ? router.query.mode === 'embed'
-                ? 'h-screen flex-grow items-center justify-center p-8 w-full'
-                : 'h-screen flex flex-grow items-center justify-center p-8 md:px-12 lg:px-24 bg-movet-white'
-              : router.pathname === '/telehealth/chat'
-              ? 'p-4 sm:p-8 bg-movet-white'
-              : 'p-4 sm:p-8 xl:px-44 bg-movet-white'
+              ? router.query.mode === "embed"
+                ? "h-screen flex-grow items-center justify-center p-8 w-full"
+                : "h-screen flex flex-grow items-center justify-center p-8 md:px-12 lg:px-24 bg-movet-white"
+              : router.pathname === "/telehealth/chat"
+              ? "p-4 sm:p-8 bg-movet-white"
+              : "p-4 sm:p-8 bg-movet-white"
           }
         >
           {children}
