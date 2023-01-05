b78ca309484cb5ad475abb98d076d32f2f507865
Alex Rodriguez <alex@alpinealex.com>
2023-01-05 16:57:20 -0600

    🐛 Bug Fixes

---
+++ apps/api.movetcare.com/src/triggers/handleAnnouncementBannerUpdate.ts
@@ -0,0 +1,101 @@
+import { sendNotification } from "../notifications/sendNotification";
+import { functions, request } from "../config/config";
+const DEBUG = true;
+export const handleAnnouncementBannerUpdate = functions.firestore
+  .document("alerts/{id}")
+  .onWrite(async (change: any, context: any) => {
+    const { id } = context.params || {};
+    const data = change.after.data();
+    if (DEBUG)
+      console.log("handleAnnouncementBannerUpdate => DATA", {
+        id,
+        data,
+      });
+    if (id === "banner") {
+      const { isActive, isActiveMobile, link, message, title } = data || {};
+      // https://vercel.com/docs/concepts/git/deploy-hooks
+      const didTriggerVercelBuildWebhook = isActive
+        ? await request
+            .post(
+              "https://api.vercel.com/v1/integrations/deploy/prj_U3YE4SJdfQooyh9TsZsZmvdoL28T/exR90BAbzS?buildCache=false"
+            )
+            .then(async (response: any) => {
+              const { data, status } = response;
+              if (DEBUG)
+                console.log(
+                  "API Response: POST https://api.vercel.com/v1/integrations/deploy/prj_U3YE4SJdfQooyh9TsZsZmvdoL28T/exR90BAbzS?buildCache=false =>",
+                  data
+                );
+              return status !== 200 && status !== 201 ? "ERROR" : data;
+            })
+            .catch(() => false)
+        : false;
+
+      sendNotification({
+        type: "slack",
+        payload: {
+          message: [
+            {
+              type: "section",
+              text: {
+                text: ":loudspeaker: _Announcement Banner Updated!_",
+                type: "mrkdwn",
+              },
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: "*TITLE:*",
+                },
+                {
+                  type: "plain_text",
+                  text: title,
+                },
+                {
+                  type: "mrkdwn",
+                  text: "*MESSAGE:*",
+                },
+                {
+                  type: "plain_text",
+                  text: message,
+                },
+                {
+                  type: "mrkdwn",
+                  text: "*LINK*",
+                },
+                {
+                  type: "plain_text",
+                  text: `https://movetcare.com${link}`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: "*IS ACTIVE:*",
+                },
+                {
+                  type: "plain_text",
+                  text:
+                    (isActive
+                      ? "WEB: :white_check_mark: "
+                      : "WEB: :red_circle:") +
+                    " | " +
+                    (isActiveMobile
+                      ? "MOBILE: :white_check_mark: "
+                      : "MOBILE: :red_circle:"),
+                },
+                {
+                  type: "mrkdwn",
+                  text: "*BUILD TRIGGERED:*",
+                },
+                {
+                  type: "plain_text",
+                  text: didTriggerVercelBuildWebhook
+                    ? ":white_check_mark:"
+                    : ":red_circle:",
+                },
+              ],
+            },
+          ],
+        },
+      });
+    }
+    return true;
+  });
