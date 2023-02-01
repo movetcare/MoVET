da075344aa5bc7a1c9cc8e522acd1be7a2d5af5b
Alex Rodriguez <alex@alpinealex.com>
2023-02-01 15:32:33 -0600

    🐛 Bug Fixes

--- apps/app.movetcare.com/components/ErrorBoundary.tsx
+++ apps/app.movetcare.com/components/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Error from "./Error";
+import { Error } from "./Error";
 class ErrorBoundary extends React.Component {
   constructor(props: any) {
     super(props);
