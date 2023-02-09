28f637dd5ed8bd52540a88f2439b47b7fe101585
Alex Rodriguez <alex@alpinealex.com>
2023-02-09 14:51:20 -0600

    🐛 Bug Fixes

--- apps/api.movetcare.com/src/endpoints/admin/accounts/deleteAccount.ts
+++ apps/api.movetcare.com/src/endpoints/admin/accounts/deleteAccount.ts
@@ -4,9 +4,9 @@ import {
   throwError,
   mobileClientApiKey,
   DEBUG,
-  admin,
+  // admin,
 } from "../../../config/config";
-import { sendNotification } from "../../../notifications/sendNotification";
+// import { sendNotification } from "../../../notifications/sendNotification";
 import { deleteAllAccountData } from "../../../utils/deleteAllAccountData";
 import { requestIsAuthorized } from "../pos/requestIsAuthorized";
 
@@ -20,42 +20,44 @@ export const deleteAccount = functions
       data?.id === undefined
     ) {
       if (DEBUG) console.log("DELETING ACCOUNT", context.auth?.uid);
-      return await admin
-        .auth()
-        .deleteUser(context.auth?.uid)
-        .then(() => {
-          sendNotification({
-            type: "slack",
-            payload: {
-              message: `:sob: ${
-                context.auth?.email || context.auth?.uid
-              } has deleted their MoVET account!`,
-            },
-          });
-          return true;
-        })
-        .catch(async (error: any) => {
-          console.log(error);
-          return await deleteAllAccountData(context.auth?.uid, false);
-        });
+      // return await admin
+      //   .auth()
+      //   .deleteUser(context.auth?.uid)
+      //   .then(() => {
+      //     sendNotification({
+      //       type: "slack",
+      //       payload: {
+      //         message: `:sob: ${
+      //           context.auth?.email || context.auth?.uid
+      //         } has deleted their MoVET account!`,
+      //       },
+      //     });
+      //     return true;
+      //   })
+      //   .catch(async (error: any) => {
+      //     console.log(error);
+      //     return await deleteAllAccountData(context.auth?.uid, false);
+      //   });
+      return await deleteAllAccountData(context.auth?.uid);
     } else if ((await requestIsAuthorized(context)) && data?.id)
-      return await admin
-        .auth()
-        .deleteUser(data?.id)
-        .then(() => {
-          sendNotification({
-            type: "slack",
-            payload: {
-              message: `:sob: ${
-                context.auth?.displayName || "An Admin"
-              } has deleted ${data.id}'s MoVET account!`,
-            },
-          });
-          return true;
-        })
-        .catch(async (error: any) => {
-          console.log(error);
-          return await deleteAllAccountData(data?.id, false);
-        });
+      // return await admin
+      //   .auth()
+      //   .deleteUser(data?.id)
+      //   .then(() => {
+      //     sendNotification({
+      //       type: "slack",
+      //       payload: {
+      //         message: `:sob: ${
+      //           context.auth?.displayName || "An Admin"
+      //         } has deleted ${data.id}'s MoVET account!`,
+      //       },
+      //     });
+      //     return true;
+      //   })
+      //   .catch(async (error: any) => {
+      //     console.log(error);
+      //     return await deleteAllAccountData(data?.id, false);
+      //   });
+      return await deleteAllAccountData(data?.id);
     else return throwError({ message: "AUTHENTICATION FAILED" });
   });
