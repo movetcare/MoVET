aa9964e96bb9496473b65433a85c82b4b5fba728
Alex Rodriguez <alex@alpinealex.com>
2023-02-23 11:44:53 -0600

    🐛 Bug Fix?

--- apps/movetcare.com/pages/_app.tsx
+++ apps/movetcare.com/pages/_app.tsx
@@ -2,7 +2,7 @@ import "styles";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import { environment } from "utilities";
+// import { environment } from "utilities";
 import dynamic from "next/dynamic";
 // import { PopUpAd } from "ui";
 
@@ -20,9 +20,10 @@ const MoVET = ({ Component, pageProps }: AppProps) => {
           content="A stress-free way to take care of your pet care. Whether your pet is at the very beginning or the tailend of their lifespan, we can provide the age-appropriate primary care they need. For puppies and kittens, we offer packages and a la carte services for nutrition advice, vaccinations and boosters, spay/neuter advice, oral care, training, microchipping, parasite control, grooming, and exercise. For senior cats and dogs, our annual checks will be tailored toward your pet's advancing years."
         />
       </Head>
-      {environment === "production" && (
+      {/* {environment === "production" && (
         <AnalyticsTracker trackerId="G-Y9896HXDFN" />
-      )}
+      )} */}
+      <AnalyticsTracker trackerId="G-Y9896HXDFN" />
       <Component {...pageProps} />
       {/* <PopUpAd
         description="Spend $30 or more in the MoVET boutique on November 25th and get a FREE veterinary exam!"
