0b9b110ac17b95ba9c7bc09439d578915e894870
Alex Rodriguez <alex@alpinealex.com>
2023-02-27 15:33:13 -0600

    🐛 Bug Fix

--- apps/movetcare.com/pages/api/k9-smiles.ts
+++ apps/movetcare.com/pages/api/k9-smiles.ts
@@ -2,5 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { processK9SmilesRequest } from "server";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return processK9SmilesRequest(req, res, "movetcare.com");
+  return processK9SmilesRequest(req, res, "movetcare.com/k9-smiles/");
 }
