ad09e998ab06a72b5a7fec43b8a18fb687e0d1ed
Alex Rodriguez <alex@alpinealex.com>
2023-03-23 15:25:41 -0500

    ⬆️ Testing api.movetcare.com Dependency Upgrades

--- apps/admin.movetcare.com/middleware.ts
+++ apps/admin.movetcare.com/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-const DEBUG = true;
+const DEBUG = false;
 export function middleware(request: NextRequest) {
   if (
     request.nextUrl.hostname === "localhost" &&
