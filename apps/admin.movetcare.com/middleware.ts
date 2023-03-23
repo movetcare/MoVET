9dba23798684368f5cedb7bec8d1b40adaceb2da
Alex Rodriguez <alex@alpinealex.com>
2023-03-23 10:53:03 -0500

    🩺 Testing Admin Deploy

--- apps/admin.movetcare.com/middleware.ts
+++ apps/admin.movetcare.com/middleware.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+const DEBUG = false;
 export function middleware(request: NextRequest) {
   if (
     request.nextUrl.hostname === "localhost" &&
     request.nextUrl.pathname === "/test/login/"
   ) {
+    if (DEBUG) console.log("Request Data =>", request);
     return NextResponse.redirect(
       "http://localhost:3002/test/login/zfBGBJfVTADzCVq2pvd8t8X8YXi4Bv4a"
     );
