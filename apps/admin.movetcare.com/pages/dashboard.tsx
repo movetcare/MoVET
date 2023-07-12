8518053d0003bc445f8fdff2acbd62333c5885e0
Alex Rodriguez <alex@alpinealex.com>
2023-07-12 13:31:33 -0500

    🐛 Updating Admin Dashboard + Android Universal Link Updates

--- apps/admin.movetcare.com/pages/dashboard.tsx
+++ apps/admin.movetcare.com/pages/dashboard.tsx
@@ -1,7 +1,6 @@
 import { ClientSearch } from "components/ClientSearch";
 import { HoursStatus } from "components/HoursStatus";
 import TelehealthChatSummary from "components/TelehealthChatSummary";
-import { Waitlist } from "components/Waitlist";
 import Head from "next/head";
 
 export default function Dashboard() {
@@ -11,9 +10,8 @@ export default function Dashboard() {
         <title>Admin Dashboard</title>
       </Head>
       <ClientSearch />
-      <HoursStatus />
       <div className="grid lg:grid-cols-2 gap-4">
-        <Waitlist />
+        <HoursStatus />
         <TelehealthChatSummary />
       </div>
     </section>
