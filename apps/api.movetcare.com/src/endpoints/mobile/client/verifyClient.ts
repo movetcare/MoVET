f8d43e1a5623490c15df11c174d01a7c3431ee46
Alex Rodriguez <alex@alpinealex.com>
2023-10-19 15:58:19 -0500

    🐛 Bug Fixes

--- apps/api.movetcare.com/src/endpoints/mobile/client/verifyClient.ts
+++ apps/api.movetcare.com/src/endpoints/mobile/client/verifyClient.ts
@@ -5,16 +5,19 @@ import { verifyExistingClient } from "../../../utils/auth/verifyExistingClient";
 
 export const verifyClient = functions
   .runWith(defaultRuntimeOptions)
-  .https.onCall(async (data: { email: string }): Promise<boolean> => {
-    const isExistingClient = await verifyExistingClient(data?.email);
-    if (isExistingClient) return true;
-    else {
-      const proVetClientData: any = await createProVetClient(data);
-      if (proVetClientData)
-        return await createAuthClient({
-          ...proVetClientData,
-          password: null,
-        });
-      else return false;
-    }
-  });
+  .https.onCall(
+    async (data: { email: string }, context: any): Promise<boolean> => {
+      console.log("verifyClient context", context);
+      const isExistingClient = await verifyExistingClient(data?.email);
+      if (isExistingClient) return true;
+      else {
+        const proVetClientData: any = await createProVetClient(data);
+        if (proVetClientData)
+          return await createAuthClient({
+            ...proVetClientData,
+            password: null,
+          });
+        else return false;
+      }
+    },
+  );
