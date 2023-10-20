3139d988b1ad5dd64a26080e2e5a9e9e5642eafd
Alex Rodriguez <alex@alpinealex.com>
2023-10-20 13:09:52 -0500

    🎨 Bug Fixes

--- apps/api.movetcare.com/src/endpoints/new/verifyClient.ts
+++ apps/api.movetcare.com/src/endpoints/new/verifyClient.ts
@@ -8,61 +8,58 @@ import { UserRecord } from "firebase-admin/lib/auth/user-record";
 
 export const verifyClient = functions
   .runWith(defaultRuntimeOptions)
-  .https.onCall(
-    async (data: { email: string }, context: any): Promise<boolean | null> => {
-      console.log("verifyClient context", JSON.stringify(context));
-      const { email } = data;
-      if (email) {
-        const isExistingClient = await verifyExistingClient(email);
-        if (isExistingClient) {
-          const authUser: UserRecord | null = await getAuthUserByEmail(email);
+  .https.onCall(async (data: { email: string }): Promise<boolean | null> => {
+    const { email } = data;
+    if (email) {
+      const isExistingClient = await verifyExistingClient(email);
+      if (isExistingClient) {
+        const authUser: UserRecord | null = await getAuthUserByEmail(email);
+        sendNotification({
+          type: "push",
+          payload: {
+            category: "admin",
+            title:
+              "New Mobile App Sign In (Existing Client) - " +
+                authUser?.displayName || email,
+            message: `${
+              authUser?.displayName || email
+            } has signed into the mobile app!`,
+            path: `/client/?id=${authUser?.uid}`,
+          },
+        });
+        return true;
+      } else {
+        const proVetClientData: any = await createProVetClient(data);
+        if (proVetClientData) {
           sendNotification({
             type: "push",
             payload: {
               category: "admin",
-              title:
-                "New Mobile App Sign In (Existing Client) - " +
-                  authUser?.displayName || email,
-              message: `${
-                authUser?.displayName || email
-              } has signed into the mobile app!`,
-              path: `/client/?id=${authUser?.uid}`,
+              title: "New Mobile App Sign Up - " + email,
+              message: `${email} has signed up for the mobile app as client #${proVetClientData?.id}`,
+              path: `/client/?id=${proVetClientData?.id}`,
             },
           });
-          return true;
+          return await createAuthClient(
+            {
+              ...proVetClientData,
+              password: null,
+            },
+            undefined,
+            false,
+          );
         } else {
-          const proVetClientData: any = await createProVetClient(data);
-          if (proVetClientData) {
-            sendNotification({
-              type: "push",
-              payload: {
-                category: "admin",
-                title: "New Mobile App Sign Up - " + email,
-                message: `${email} has signed up for the mobile app as client #${proVetClientData?.id}`,
-                path: `/client/?id=${proVetClientData?.id}`,
-              },
-            });
-            return await createAuthClient(
-              {
-                ...proVetClientData,
-                password: null,
-              },
-              undefined,
-              false,
-            );
-          } else {
-            sendNotification({
-              type: "push",
-              payload: {
-                category: "admin",
-                title: "FAILED Mobile App Sign Up - " + email,
-                message: `${email} has FAILED to sign up for the mobile app...`,
-                path: `/client/?id=${proVetClientData?.id}`,
-              },
-            });
-            return false;
-          }
+          sendNotification({
+            type: "push",
+            payload: {
+              category: "admin",
+              title: "FAILED Mobile App Sign Up - " + email,
+              message: `${email} has FAILED to sign up for the mobile app...`,
+              path: `/client/?id=${proVetClientData?.id}`,
+            },
+          });
+          return false;
         }
-      } else return null;
-    },
-  );
+      }
+    } else return null;
+  });
