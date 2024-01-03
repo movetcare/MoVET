7df92be0d3cdf0981406c179750af040c4cdc83a
Alex Rodriguez <alex@alpinealex.com>
2024-01-03 11:39:01 -0600

    🎨 Bug Fix

--- apps/movetcare.com/pages/emergency.tsx
+++ apps/movetcare.com/pages/emergency.tsx
@@ -14,36 +14,35 @@ export async function getStaticProps() {
     } as any,
   };
 }
+const EmergencyPage = ({ popUpAd }: { popUpAd: PopUpAdType }) => (
+  <Layout>
+    <Head>
+      <title>Emergency Care Notice</title>
+    </Head>
+    <section className="relative max-w-screen-lg bg-white rounded-xl p-4 sm:p-8 mx-4 sm:mx-auto my-4 sm:m-8">
+      <EmergencyWarning />
+    </section>
+    {popUpAd?.isActive && (
+      <PopUpAd
+        autoOpen={popUpAd?.autoOpen}
+        icon={popUpAd?.icon}
+        title={popUpAd?.title}
+        description={popUpAd?.description}
+        adComponent={
+          <Link href={popUpAd?.link as string}>
+            <Image
+              className="rounded-xl"
+              src={popUpAd?.imagePath as string}
+              alt={popUpAd?.title}
+              height={popUpAd?.height || 200}
+              width={popUpAd?.width || 200}
+            />
+          </Link>
+        }
+        ignoreUrlPath={popUpAd?.ignoreUrlPath}
+      />
+    )}
+  </Layout>
+);
 
-export default function EmergencyPage({ popUpAd }: { popUpAd: PopUpAdType }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Emergency Care Notice</title>
-      </Head>
-      <section className="relative max-w-screen-lg bg-white rounded-xl p-4 sm:p-8 mx-4 sm:mx-auto my-4 sm:m-8">
-        <EmergencyWarning />
-      </section>
-      {popUpAd?.isActive && (
-        <PopUpAd
-          autoOpen={popUpAd?.autoOpen}
-          icon={popUpAd?.icon}
-          title={popUpAd?.title}
-          description={popUpAd?.description}
-          adComponent={
-            <Link href={popUpAd?.link as string}>
-              <Image
-                className="rounded-xl"
-                src={popUpAd?.imagePath as string}
-                alt={popUpAd?.title}
-                height={popUpAd?.height || 200}
-                width={popUpAd?.width || 200}
-              />
-            </Link>
-          }
-          ignoreUrlPath={popUpAd?.ignoreUrlPath}
-        />
-      )}
-    </Layout>
-  );
-}
+export default EmergencyPage;
