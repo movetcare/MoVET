4bc7fe137c0f998207dde18611f02481dcd40169
Alex Rodriguez <alex@alpinealex.com>
2024-02-29 13:43:49 -0600

    Upgrading Dependencies

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -15,10 +15,6 @@ const Index = () => {
         router.replace("/sign-in");
       else if (isLoggedIn && segments.length === 0)
         router.replace("/(app)/home");
-      else if (isLoggedIn && segments.length > 0) {
-        //alert("segments => " + JSON.stringify(segments));
-        router.replace("/(app)/chat");
-      }
     }
   }, [segments, navigationState?.key, initialized, isLoggedIn]);
 
