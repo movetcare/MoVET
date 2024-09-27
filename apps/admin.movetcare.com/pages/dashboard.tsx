4d851fd2d0608cc178e7f880e8f7928e9276538a
Alex Rodriguez <alex@alpinealex.com>
2024-09-27 14:02:38 -0500

    💚 Trigger Deployment?

--- apps/admin.movetcare.com/pages/dashboard.tsx
+++ apps/admin.movetcare.com/pages/dashboard.tsx
@@ -5,6 +5,8 @@ import { PushNotificationWarning } from "components/PushNotificationWarning";
 import TelehealthChatSummary from "components/TelehealthChatSummary";
 import Head from "next/head";
 
+// TODO: This is just to trigger a deployment on Vercel.
+
 export default function Dashboard() {
   return (
     <section>
